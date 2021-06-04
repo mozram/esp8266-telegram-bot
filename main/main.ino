47e1f06a3029bd4abb4cf9c16f7e5f1f63e04b75
Zulhilmi Ramli <mohdzulhilmi.ramli@dyson.com>
2021-06-04 23:54:51 +0800

    Working prototype

--- main/main.ino
+++ main/main.ino
@@ -20,6 +20,9 @@
 const char* ssid = "Ramli_wifi";
 const char* password = "78933476";
 
+#define SWITCH_ON   LOW
+#define SWITCH_OFF  HIGH
+
 // Initialize Telegram BOT
 #define BOTtoken "1839545543:AAFr93e36zCde2dJkyCYL7uTTokKPhxPe_w"  // your Bot Token (Get from Botfather)
 
@@ -39,8 +42,12 @@ UniversalTelegramBot bot(BOTtoken, client);
 int botRequestDelay = 1000;
 unsigned long lastTimeBotRan;
 
-const int ledPin = 2;
-bool ledState = LOW;
+// const int kipasPin = 2;
+const int kipasPin = 4;
+bool kipasState = SWITCH_OFF;
+
+const int lampuPin = 5;
+bool lamputState = SWITCH_OFF;
 
 // Handle what happens when you receive new messages
 void handleNewMessages(int numNewMessages) {
@@ -61,40 +68,82 @@ void handleNewMessages(int numNewMessages) {
 
     String from_name = bot.messages[i].from_name;
 
-    if (text == "/start") {
-      String welcome = "Welcome, " + from_name + ".\n";
-      welcome += "Use the following commands to control your outputs.\n\n";
-      welcome += "/led_on to turn GPIO ON \n";
-      welcome += "/led_off to turn GPIO OFF \n";
-      welcome += "/state to request current GPIO state \n";
-      bot.sendMessage(chat_id, welcome, "");
-    }
-
-    if (text == "/led_on") {
-      bot.sendMessage(chat_id, "LED state set to ON", "");
-      ledState = HIGH;
-      digitalWrite(ledPin, ledState);
-    }
-    
-    if (text == "/led_off") {
-      bot.sendMessage(chat_id, "LED state set to OFF", "");
-      ledState = LOW;
-      digitalWrite(ledPin, ledState);
-    }
-    
-    if (text == "/state") {
-      if (digitalRead(ledPin)){
-        bot.sendMessage(chat_id, "LED is ON", "");
-      }
-      else{
-        bot.sendMessage(chat_id, "LED is OFF", "");
-      }
-    }
-
-    if (text == "/options")
+    // Inline buttons with callbacks when pressed will raise a callback_query message
+    if (bot.messages[i].type == "callback_query")
     {
-      String keyboardJson = "[[{ \"text\" : \"Go to Google\", \"url\" : \"https://www.google.com\" }],[{ \"text\" : \"Send\", \"callback_data\" : \"This was sent by inline\" }]]";
-      bot.sendMessageWithInlineKeyboard(chat_id, "Choose from one of the following options", "", keyboardJson);
+      Serial.print("Call back button pressed by: ");
+      Serial.println(bot.messages[i].from_id);
+      Serial.print("Data on the button: ");
+      Serial.println(bot.messages[i].text);
+
+      if (text == "Kipas ON")
+      {
+        kipasState = SWITCH_ON;
+        digitalWrite(kipasPin, kipasState);
+      }
+
+      if (text == "Kipas OFF")
+      {
+        kipasState = SWITCH_OFF;
+        digitalWrite(kipasPin, kipasState);
+      }
+
+      if (text == "Lampu ON")
+      {
+        lamputState = SWITCH_ON;
+        digitalWrite(lampuPin, lamputState);
+      }
+
+      if (text == "Lampu OFF")
+      {
+        lamputState = SWITCH_OFF;
+        digitalWrite(lampuPin, lamputState);
+      }
+
+      bot.sendMessage(bot.messages[i].from_id, bot.messages[i].text, "");
+    }
+    else
+    {
+      if (text == "/start") {
+        String welcome = "Welcome, " + from_name + ".\n";
+        welcome += "Use the following commands to control your outputs.\n\n";
+        welcome += "/led_on to turn GPIO ON \n";
+        welcome += "/led_off to turn GPIO OFF \n";
+        welcome += "/state to request current GPIO state \n";
+        bot.sendMessage(chat_id, welcome, "");
+      }
+
+      if (text == "/led_on")
+      {
+        bot.sendMessage(chat_id, "LED state set to ON", "");
+        kipasState = SWITCH_OFF;
+        digitalWrite(kipasPin, kipasState);
+      }
+      
+      if (text == "/led_off")
+      {
+        bot.sendMessage(chat_id, "LED state set to OFF", "");
+        kipasState = SWITCH_ON;
+        digitalWrite(kipasPin, kipasState);
+      }
+      
+      if (text == "/state")
+      {
+        if (digitalRead(kipasPin))
+        {
+          bot.sendMessage(chat_id, "LED is ON", "");
+        }
+        else
+        {
+          bot.sendMessage(chat_id, "LED is OFF", "");
+        }
+      }
+
+      if (text == "/options")
+      {
+        String keyboardJson = "[[{ \"text\" : \"Kipas ON\", \"callback_data\" : \"Kipas ON\" }],[{ \"text\" : \"Kipas OFF\", \"callback_data\" : \"Kipas OFF\" }],[{ \"text\" : \"Lampu ON\", \"callback_data\" : \"Lampu ON\" }],[{ \"text\" : \"Lampu OFF\", \"callback_data\" : \"Lampu OFF\" }]]";
+        bot.sendMessageWithInlineKeyboard(chat_id, "Choose from one of the following options", "", keyboardJson);
+      }
     }
   }
 }
@@ -107,8 +156,10 @@ void setup() {
     client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
   #endif
 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, ledState);
+  pinMode(kipasPin, OUTPUT);
+  digitalWrite(kipasPin, kipasState);
+  pinMode(lampuPin, OUTPUT);
+  digitalWrite(lampuPin, lamputState);
   
   // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
